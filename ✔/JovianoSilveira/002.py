3e5ef94725e6d3e40ad0ace488dacea271239fc7
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-03-11 16:43:01 -0300

    update

---
+++ ✔/JovianoSilveira/002.py
@@ -0,0 +1,9 @@
+# While
+while True:
+    passOrigin = input('Digite sua senha: ')
+    senha = passOrigin.lower()
+    if senha == 'flame':
+        print('e')
+        break
+    else:
+        print("senha errada")
