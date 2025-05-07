5758e77f73ba26c53a5148435a4e712651c0fc8f
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-05-06 21:17:20 -0300

    update

---
+++ Aulas/sites/Sololearn/001/main.py
@@ -0,0 +1,4 @@
+# listas!
+games = ["Snake", "Puzzle", "Shooter"]
+"Complete o código abaixo para substituir Puzzle por Race"
+games[1] = 'Race'
--- Aulas/videos-yt/BosonTreinamentos/aula014/main.py
+++ Aulas/videos-yt/BosonTreinamentos/aula014/main.py
@@ -1,9 +1,13 @@
 # Escrita: w
 arquivo = open('arquivo.txt', 'w', encoding='utf-8')
-arquivo.write('João')
+arquivo.write('Hello, World!')
 arquivo.close()
 
 # Leitura: r
 arquivo2 = open('arquivo.txt', 'r', encoding='utf-8')
 print(arquivo2.read())
 arquivo2.close()
+
+# Acrescentar: a
+with open('arquivo.txt', 'a', encoding='utf-8') as arquivo:
+    arquivo.write('Olá, Mundo!')
