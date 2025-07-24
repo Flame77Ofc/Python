47dc90b8035a6ae2baa4bddc48cdf0fa9ac263b3
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-07-24 20:43:35 -0300

    pygame aulas

---
+++ Outros/Games/Pygame/ProgrammingKnowledge/aulas/002.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Meu Jogo")
+
+fps = pygame.time.Clock()
+
+running = True
+while running:
+    fps.tick(60)
+    bg = screen.fill((34, 89, 76))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+pygame.quit()
