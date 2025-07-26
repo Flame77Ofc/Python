69bd521e50b1c3d507af2fd0be52e122dbe4ef70
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-07-26 12:47:24 -0300

    bot updating refactoring

--- Outros/webScraping/bs4/bot/main/wikipedia/main.py
+++
@@ -1,4 +0,0 @@
-assunto = input('Digite o assunto que deseja pesquisar: ').strip()
-
-from .wikipedia import wikipedia
-wikipedia(assunto)
--- Outros/webScraping/bs4/bot/main/zlibrary/main.py
+++
@@ -1,4 +0,0 @@
-assunto = input("Digite o assunto do livro:\n>>>")
-from .zlibrary import pegar_livros
-
-pegar_livros(assunto)
